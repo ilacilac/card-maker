f57888fe1639553b92590aa0451c4a673b540efc
ilacilac <qkralswl307@gmail.com>
2021-01-14 00:36:05 +0900

    feat: project set up & add firebase

---
+++ src/service/auth_service.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+class AuthService {
+  login(providerName) {
+    const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+    return firebase.auth().signInWithPopup(authProvider);
+  }
+}
+
+export default AuthService;
---
+++ src/service/firebase.js
@@ -0,0 +1,9 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
